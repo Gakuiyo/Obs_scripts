f678a6a65e22d613948290098a24933e4d7d774c
Gakuiyo <105859837+Gakuiyo@users.noreply.github.com>
2022-05-19 14:31:35 +0200

    Count script for OBS
    Use a keybind to increse or decrease a counter for your stream

---
+++ SimpleCounterOBS.lua
@@ -0,0 +1,280 @@
+obs           = obslua
+source_name   = ""
+
+last_text     = ""
+activated     = false
+
+wins  = 0
+loses = 0
+
+hotkey_id     = obs.OBS_INVALID_HOTKEY_ID
+
+hotkey_WinPlus_id     = obs.OBS_INVALID_HOTKEY_ID
+hotkey_WinMinus_id     = obs.OBS_INVALID_HOTKEY_ID
+
+hotkey_LosePlus_id     = obs.OBS_INVALID_HOTKEY_ID
+hotkey_LoseMinus_id     = obs.OBS_INVALID_HOTKEY_ID
+
+-- Function to set the time text
+function set_time_text()
+	local text          = string.format("%d", wins)
+
+	if text ~= last_text then
+		local source = obs.obs_get_source_by_name(source_name)
+		if source ~= nil then
+			local settings = obs.obs_data_create()
+			obs.obs_data_set_string(settings, "text", text)
+			obs.obs_source_update(source, settings)
+			obs.obs_data_release(settings)
+			obs.obs_source_release(source)
+		end
+	end
+
+	last_text = text
+end
+
+function timer_callback()
+	set_time_text()
+end
+
+function activate(activating)
+	if activated == activating then
+		return
+	end
+
+	activated = activating
+
+	if activating then
+		set_time_text()
+	end
+end
+
+-- Called when a source is activated/deactivated
+function activate_signal(cd, activating)
+	local source = obs.calldata_source(cd, "source")
+	if source ~= nil then
+		local name = obs.obs_source_get_name(source)
+		if (name == source_name) then
+			activate(activating)
+		end
+	end
+end
+
+function source_activated(cd)
+	activate_signal(cd, true)
+end
+
+function source_deactivated(cd)
+	activate_signal(cd, false)
+end
+
+function reset(pressed)
+	if not pressed then
+		return
+	end
+
+	activate(false)
+	local source = obs.obs_get_source_by_name(source_name)
+	if source ~= nil then
+		local active = obs.obs_source_active(source)
+		obs.obs_source_release(source)
+		activate(active)
+	end
+end
+
+function Plus(pressed)
+	if not pressed then
+		return
+	end
+
+	activate(false)
+	wins = wins + 1
+	set_time_text()
+end
+
+function Minus(pressed)
+	if not pressed then
+		return
+	end
+
+	activate(false)
+	wins = wins - 1
+	set_time_text()
+end
+
+function Plus5(pressed)
+	if not pressed then
+		return
+	end
+
+	activate(false)
+	wins = wins + 5
+	set_time_text()
+end
+
+function Minus5(pressed)
+	if not pressed then
+		return
+	end
+
+	activate(false)
+	wins = wins - 5
+	set_time_text()
+end
+
+function Plus10(pressed)
+	if not pressed then
+		return
+	end
+
+	activate(false)
+	wins = wins + 10
+	set_time_text()
+end
+
+function Minus10(pressed)
+	if not pressed then
+		return
+	end
+
+	activate(false)
+	wins = wins - 10
+	set_time_text()
+end
+
+function reset_button_clicked(props, p)
+	reset(true)
+	return false
+end
+
+----------------------------------------------------------
+
+-- A function named script_properties defines the properties that the user
+-- can change for the entire script module itself
+function script_properties()
+	local props = obs.obs_properties_create()
+
+	local p = obs.obs_properties_add_list(props, "source", "Text Source", obs.OBS_COMBO_TYPE_EDITABLE, obs.OBS_COMBO_FORMAT_STRING)
+	local sources = obs.obs_enum_sources()
+	if sources ~= nil then
+		for _, source in ipairs(sources) do
+			source_id = obs.obs_source_get_unversioned_id(source)
+			if source_id == "text_gdiplus" or source_id == "text_ft2_source" then
+				local name = obs.obs_source_get_name(source)
+				obs.obs_property_list_add_string(p, name, name)
+			end
+		end
+	end
+	obs.source_list_release(sources)
+	obs.obs_properties_add_button(props, "reset_button", "Reset Count", reset_button_clicked)
+
+	return props
+end
+
+-- A function named script_description returns the description shown to
+-- the user
+function script_description()
+	return "Sets a text source to act as a custom counter when the source is active.\n\nMade by Gakuiyo, and use code from countdown included with OBS by Jim"
+end
+
+-- A function named script_update will be called when settings are changed
+function script_update(settings)
+	activate(false)
+
+	loses = 0
+	wins = 0
+	source_name = obs.obs_data_get_string(settings, "source")
+
+	reset(true)
+end
+
+-- A function named script_defaults will be called to set the default settings
+function script_defaults(settings)
+
+end
+
+-- A function named script_save will be called when the script is saved
+--
+-- NOTE: This function is usually used for saving extra data (such as in this
+-- case, a hotkey's save data).  Settings set via the properties are saved
+-- automatically.
+function script_save(settings)
+	local hotkey_save_array = obs.obs_hotkey_save(hotkey_id)
+	obs.obs_data_set_array(settings, "reset_hotkey", hotkey_save_array)
+	obs.obs_data_array_release(hotkey_save_array)
+
+
+	local hotkey_save_array = obs.obs_hotkey_save(hotkey_WinPlus_id)
+	obs.obs_data_set_array(settings, "Plus_hotkey", hotkey_save_array)
+	obs.obs_data_array_release(hotkey_save_array)
+
+	local hotkey_save_array = obs.obs_hotkey_save(hotkey_WinMinus_id)
+	obs.obs_data_set_array(settings, "Minus_hotkey", hotkey_save_array)
+	obs.obs_data_array_release(hotkey_save_array)
+
+	local hotkey_save_array = obs.obs_hotkey_save(hotkey_WinPlus5_id)
+	obs.obs_data_set_array(settings, "Plus5_hotkey", hotkey_save_array)
+	obs.obs_data_array_release(hotkey_save_array)
+
+	local hotkey_save_array = obs.obs_hotkey_save(hotkey_WinMinus5_id)
+	obs.obs_data_set_array(settings, "Minus5_hotkey", hotkey_save_array)
+	obs.obs_data_array_release(hotkey_save_array)
+
+	local hotkey_save_array = obs.obs_hotkey_save(hotkey_WinPlus10_id)
+	obs.obs_data_set_array(settings, "Plus10_hotkey", hotkey_save_array)
+	obs.obs_data_array_release(hotkey_save_array)
+
+	local hotkey_save_array = obs.obs_hotkey_save(hotkey_WinMinus10_id)
+	obs.obs_data_set_array(settings, "Minus10_hotkey", hotkey_save_array)
+	obs.obs_data_array_release(hotkey_save_array)
+end
+
+-- a function named script_load will be called on startup
+function script_load(settings)
+	-- Connect hotkey and activation/deactivation signal callbacks
+	--
+	-- NOTE: These particular script callbacks do not necessarily have to
+	-- be disconnected, as callbacks will automatically destroy themselves
+	-- if the script is unloaded.  So there's no real need to manually
+	-- disconnect callbacks that are intended to last until the script is
+	-- unloaded.
+	local sh = obs.obs_get_signal_handler()
+	obs.signal_handler_connect(sh, "source_activate", source_activated)
+	obs.signal_handler_connect(sh, "source_deactivate", source_deactivated)
+
+	hotkey_id = obs.obs_hotkey_register_frontend("reset_timer_thingy", "Reset Count", reset)
+	local hotkey_save_array = obs.obs_data_get_array(settings, "reset_hotkey")
+	obs.obs_hotkey_load(hotkey_id, hotkey_save_array)
+	obs.obs_data_array_release(hotkey_save_array)
+
+
+	hotkey_WinPlus_id = obs.obs_hotkey_register_frontend("plus_thingy", "Plus", Plus)
+	local hotkey_save_array = obs.obs_data_get_array(settings, "Plus_hotkey")
+	obs.obs_hotkey_load(hotkey_WinPlus_id, hotkey_save_array)
+	obs.obs_data_array_release(hotkey_save_array)
+
+	hotkey_WinMinus_id = obs.obs_hotkey_register_frontend("minus_thingy", "Minus", Minus)
+	local hotkey_save_array = obs.obs_data_get_array(settings, "Minus_hotkey")
+	obs.obs_hotkey_load(hotkey_WinMinus_id, hotkey_save_array)
+	obs.obs_data_array_release(hotkey_save_array)
+
+	hotkey_WinPlus5_id = obs.obs_hotkey_register_frontend("plus5_thingy", "Plus5", Plus5)
+	local hotkey_save_array = obs.obs_data_get_array(settings, "Plus5_hotkey")
+	obs.obs_hotkey_load(hotkey_WinPlus5_id, hotkey_save_array)
+	obs.obs_data_array_release(hotkey_save_array)
+
+	hotkey_WinMinus5_id = obs.obs_hotkey_register_frontend("minus5_thingy", "Minus5", Minus5)
+	local hotkey_save_array = obs.obs_data_get_array(settings, "Minus5_hotkey")
+	obs.obs_hotkey_load(hotkey_WinMinus5_id, hotkey_save_array)
+	obs.obs_data_array_release(hotkey_save_array)
+
+	hotkey_WinPlus10_id = obs.obs_hotkey_register_frontend("plus10_thingy", "Plus10", Plus10)
+	local hotkey_save_array = obs.obs_data_get_array(settings, "Plus10_hotkey")
+	obs.obs_hotkey_load(hotkey_WinPlus10_id, hotkey_save_array)
+	obs.obs_data_array_release(hotkey_save_array)
+
+	hotkey_WinMinus10_id = obs.obs_hotkey_register_frontend("minus10_thingy", "Minus10", Minus10)
+	local hotkey_save_array = obs.obs_data_get_array(settings, "Minus10_hotkey")
+	obs.obs_hotkey_load(hotkey_WinMinus10_id, hotkey_save_array)
+	obs.obs_data_array_release(hotkey_save_array)
+end
